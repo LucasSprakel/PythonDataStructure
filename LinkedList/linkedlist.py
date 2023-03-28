b29dd7f64386cd5395f3b7d5100c099c81839243
Lucas Sprakel <53552116+LucasSprakel@users.noreply.github.com>
2023-03-28 18:20:47 -0300

    Update linkedlist.py

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -4,19 +4,21 @@ class Node:
         self.next = None
 
 
-class List:
+class LinkedList:
     def __init__(self):
         self.head = None
         self.size = 0
 
+    
     def list_is_empty(self):
         if self.size == 0:
             return True
         return False
     
+    
     def insert(self, value, index):
         if index > self.size + 1 or index <= 0:
-            raise IndexError("Invalid Index")
+            raise IndexError("Invalid Index. Insira um indice válido, caso a lista por exemplo so tenha um elemento, não pode inserir no indice 3.")
         
         node = Node(value)
         
@@ -27,27 +29,47 @@ class List:
             pointer = self.head
             for i in range(1, index-1):
                 pointer = pointer.next
+            node.next = pointer.next
             pointer.next = node
-            pointer = node
-        
-        self.size += 1
             
+        self.size += 1
 
+    
+    def remove(self, index):
+        if index > self.size or index <= 0:
+            raise IndexError("Invalid Index. Para remover um elemento, a posição(indice) tem que existir")
+        
+        pointer = self.head
+        aux = None
+        for i in range(1, index):
+            aux = pointer
+            pointer = pointer.next
+        
+        aux.next = pointer.next
+
+        self.size -= 1
+    
+    
     def show_element(self, index):
         if self.list_is_empty() or index > self.size or index <= 0:
             raise IndexError("Invalid Index")
 
         pointer = self.head
         for i in range(1, index):
-            if pointer:
-                pointer = pointer.next
-            else:
-                raise IndexError("error")
+            pointer = pointer.next
         
-        if pointer:
-            return pointer.value
-        else:
-            raise IndexError("error")
+        print(pointer.value)
+
+    
+    def edit_element(self, index, value):
+        if self.list_is_empty() or index > self.size or index <= 0:
+            raise IndexError("Invalid Index")
+
+        pointer = self.head
+        for i in range(1, index):
+            pointer = pointer.next
+
+        pointer.value = value
         
     
     def show_list(self):
@@ -55,15 +77,4 @@ class List:
         while pointer:
             print(pointer.value)
             pointer = pointer.next
-
-    
-lista = List()
-lista.insert(50, 1)
-lista.insert(30, 2)
-lista.insert(40, 3)
-lista.insert(60, 4)
-lista.insert(80, 3)
-lista.insert(70, 2)
-lista.show_list()
-    
 
